d65f3524a0ff32175af6a70b5fca28838815aae1
Seo Jaegon <jgoneit@gmail.com>
2023-07-04 19:34:34 +0900

    test: faRecRepo & Service Test ver.1 complete

--- server/src/test/java/com/zerohip/server/financialRecord/service/FinancialRecordServiceImplTest.java
+++ server/src/test/java/com/zerohip/server/financialRecord/service/FinancialRecordServiceImplTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -61,7 +63,7 @@ class FinancialRecordServiceImplTest {
 
   @Test
   @DisplayName("가계부 전체 조회(동적쿼리 사용 예정)")
-  void findAllFaRecs() {
+  void findFaRecs() {
     // 생성된 인스턴스의 정보로 가계부 생성 후 saveFaRec에 저장
     FinancialRecord saveFaRec1 = financialRecordService.createFaRec(faRec);
 
@@ -71,11 +73,17 @@ class FinancialRecordServiceImplTest {
     log.info("saveFaRec1.getFinancialRecordName() = {}", saveFaRec1.getFinancialRecordName());
     log.info("saveFaRec2.getFinancialRecordName() = {}", saveFaRec2.getFinancialRecordName());
 
-    // 생성된 가계부의 id로 조회
-    financialRecordService.findAllFaRecs();
 
     // Verify
-//    assertThat(financialRecordService.findAllFaRecs());
+    findFaRecsTest(saveFaRec1, saveFaRec2);
+  }
+
+  // 가계부 전체 조회 테스트 메서드
+  // '...'는 가변 인자(varags)로 메소드에 임의 개수의 동일타입의 매개변수를 전달
+  void findFaRecsTest(FinancialRecord... faRecs) {
+    List<FinancialRecord> result = financialRecordService.findFaRecs();
+    assertThat(result.size()).isEqualTo(faRecs.length);
+    assertThat(result).containsExactly(faRecs);
   }
 
   @Test
