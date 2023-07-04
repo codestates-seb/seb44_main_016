7a636ca359a82ae4541d57b35bcc629867e4f897
Seo Jaegon <jgoneit@gmail.com>
2023-07-04 13:33:11 +0900

    feat: Add Test Code About FinancialRecord

--- server/src/test/java/com/zerohip/server/financialRecord/repository/FinancialRecordRepositoryTest.java
+++ server/src/test/java/com/zerohip/server/financialRecord/repository/FinancialRecordRepositoryTest.java
@@ -1,4 +1,34 @@
+package com.zerohip.server.financialRecord.repository;
+
+import com.zerohip.server.financialRecord.entity.FinancialRecord;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Transactional
+@SpringBootTest
 class FinancialRecordRepositoryTest {
-  
+
+  @Autowired
+  FinancialRecordRepository financialRecordRepository;
+
+  @Test
+  void createFaRec() {
+    FinancialRecord faRec = new FinancialRecord("test용 가계부");
+    FinancialRecord saveFaRec = financialRecordRepository.save(faRec);
+    log.info("saveFaRec.getFinancialRecordName() = {}", saveFaRec.getFinancialRecordName());
+    Optional<FinancialRecord> optionalFaRec = financialRecordRepository.findById(saveFaRec.getFinancialRecordId());
+    FinancialRecord findFaRec = optionalFaRec.orElseThrow(() -> new RuntimeException("가계부가 없습니다."));
+
+    assertEquals(faRec.getFinancialRecordName(), saveFaRec.getFinancialRecordName());
+    assertEquals(saveFaRec.getFinancialRecordName(), findFaRec.getFinancialRecordName());
+  }
+
 }
--- server/src/test/java/com/zerohip/server/financialRecord/service/FinancialRecordServiceImplTest.java
+++ server/src/test/java/com/zerohip/server/financialRecord/service/FinancialRecordServiceImplTest.java
@@ -1,4 +1,28 @@
+package com.zerohip.server.financialRecord.service;
+
+import com.zerohip.server.financialRecord.entity.FinancialRecord;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Transactional
+@SpringBootTest
 class FinancialRecordServiceImplTest {
-  
+
+  @Autowired
+  FinancialRecordService financialRecordService;
+
+  @Test
+  void createFaRec() {
+    FinancialRecord faRec = new FinancialRecord("test용 가계부");
+    FinancialRecord saveFaRec = financialRecordService.createFaRec(faRec);
+    log.info("saveFaRec.getFinancialRecordName() = {}", saveFaRec.getFinancialRecordName());
+
+    assertEquals(faRec.getFinancialRecordName(), saveFaRec.getFinancialRecordName());
+  }
 }
