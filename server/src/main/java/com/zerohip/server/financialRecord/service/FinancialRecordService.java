381aabc26cb58c109b4834cea6dfbd566b751744
Seo Jaegon <jgoneit@gmail.com>
2023-07-04 09:54:39 +0900

    init: Start Project faRec With this branch

--- server/src/main/java/com/zerohip/server/financialRecord/service/FinancialRecordService.java
+++ server/src/main/java/com/zerohip/server/financialRecord/service/FinancialRecordService.java
@@ -11,7 +11,7 @@ public interface FinancialRecordService {
   // 가계부 조회(단건)
   FinancialRecord findFaRec(Long faRecId);
 
-  // 가계부 조회(전체)
+  // 가계부 전체 조회(동적쿼리 사용 예정)
   FinancialRecord findAllFaRecs();
 
   // 가계부 수정
