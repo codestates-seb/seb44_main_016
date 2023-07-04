99e38afac608ddbf154ec625892bf28f4508052d
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-04 16:20:01 +0900

    chore: svg 로고 및 관련 설정 추가

--- client/next.config.js
+++ client/next.config.js
@@ -5,4 +5,13 @@ const withTwin = require('./withTwin.js');
  */
 module.exports = withTwin({
   reactStrictMode: true,
+  webpack: (config) => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 });
