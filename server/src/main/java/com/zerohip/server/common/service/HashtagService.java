a1c55bf0efaf4b9d007e254d3aab89d0b0ab7f01
YOONHEE-KIM <yhgim1127@gmail.com>
2023-07-04 14:19:17 +0900

    id 타입 수정

--- server/src/main/java/com/zerohip/server/common/service/HashtagService.java
+++ server/src/main/java/com/zerohip/server/common/service/HashtagService.java
@@ -3,10 +3,10 @@ package com.zerohip.server.common.service;
 import java.util.List;
 
 public interface HashtagService {
-    void addHashtagToFeedArticle(String feedArticleId, String Hashtag);
-    void addHashtagToFinancialRecordArticle(String financialRecordArticleId, String hashtag);
-    List<String> getHashtagsForFeedArticle(String feedArticleId);
-    List<String> getHashtagsForFinancialRecordArticle(String financialRecordArticleId);
-    void removeHashtagFromFeedArticle(String feedArticleId, String hashtag);
-    void removeHashtagFromFinancialRecordArticle(String financialRecordArticleId, String hashtag);
+    void addHashtagToFeedArticle(Long feedArticleId, String Hashtag);
+    void addHashtagToFinancialRecordArticle(Long financialRecordArticleId, String hashtag);
+    List<String> getHashtagsForFeedArticle(Long feedArticleId);
+    List<String> getHashtagsForFinancialRecordArticle(Long financialRecordArticleId);
+    void removeHashtagFromFeedArticle(Long feedArticleId, String hashtag);
+    void removeHashtagFromFinancialRecordArticle(Long financialRecordArticleId, String hashtag);
 }
