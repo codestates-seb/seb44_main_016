4d4542e051208efa7e6fa728445ead9eb28c2fa3
Seo Jaegon <jgoneit@gmail.com>
2023-07-04 11:43:14 +0900

    feat: Add FianacialRecordDto

---
+++ server/src/main/java/com/zerohip/server/financialRecord/dto/FinancialRecordDto.java
@@ -0,0 +1,44 @@
+package com.zerohip.server.financialRecord.dto;
+
+import com.zerohip.server.common.audit.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class FinancialRecordDto {
+
+  @Getter
+  @AllArgsConstructor
+  public static class Post extends Auditable {
+    @NotBlank
+    @Size(min = 1, max = 30)
+    @Column(nullable = false, unique = false, updatable = true)
+    private String financialRecordName;
+
+    // User
+  }
+
+  @Getter
+  @AllArgsConstructor
+  public static class Patch extends Auditable {
+    private Long financialRecordId;
+    @NotBlank
+    @Size(min = 1, max = 30)
+    @Column(nullable = false, unique = false, updatable = true)
+    private String financialRecordName;
+
+    // User
+  }
+
+  @Getter
+  @AllArgsConstructor
+  public static class Response extends Auditable {
+    private Long financialRecordId;
+    private String financialRecordName;
+
+    // User
+  }
+}
