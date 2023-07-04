5cc3b85983001846760cfcdc64087543c1eda5d1
Seo Jaegon <jgoneit@gmail.com>
2023-07-04 11:20:53 +0900

    feat: Add Basic Field of FinancialRecord

--- server/src/main/java/com/zerohip/server/financialRecord/entity/FinancialRecord.java
+++ server/src/main/java/com/zerohip/server/financialRecord/entity/FinancialRecord.java
@@ -1,7 +1,29 @@
 package com.zerohip.server.financialRecord.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Setter
 @Getter
+@Entity
 public class FinancialRecord {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long financialRecordId;
+
+  @NotBlank
+  @Size(min = 1, max = 30)
+  @Column(nullable = false, unique = false, updatable = true)
+  private String financialRecordName;
+
+  // 유저 매핑
+
+  // 친구? 그룹도 필요
 }
