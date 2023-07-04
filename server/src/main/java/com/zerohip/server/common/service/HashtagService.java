463bc59e517c6ad1c64d292aaac42d1f2d51b8d3
YOONHEE-KIM <yhgim1127@gmail.com>
2023-07-04 15:32:46 +0900

    financialRecordArticleId -> faRecArticleId

--- server/src/main/java/com/zerohip/server/common/service/HashtagService.java
+++ server/src/main/java/com/zerohip/server/common/service/HashtagService.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public interface HashtagService {
     void addHashtagToFeedArticle(Long feedArticleId, String Hashtag);
-    void addHashtagToFinancialRecordArticle(Long financialRecordArticleId, String hashtag);
+    void addHashtagToFinancialRecordArticle(Long faRecArticleId, String hashtag);
     List<String> getHashtagsForFeedArticle(Long feedArticleId);
-    List<String> getHashtagsForFinancialRecordArticle(Long financialRecordArticleId);
+    List<String> getHashtagsForFinancialRecordArticle(Long faRecArticleId);
     void removeHashtagFromFeedArticle(Long feedArticleId, String hashtag);
-    void removeHashtagFromFinancialRecordArticle(Long financialRecordArticleId, String hashtag);
+    void removeHashtagFromFinancialRecordArticle(Long faRecArticleId, String hashtag);
 }
