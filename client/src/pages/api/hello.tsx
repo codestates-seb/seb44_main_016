20f5365aafd76f398df8b80419c36aef4a37b8c6
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-05 18:38:51 +0900

    test: 테스트 서버 구축

---
+++ client/src/pages/api/hello.tsx
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    pokemon: {
+      name: '꼬부기',
+    },
+  });
+}
