97b7d19a3dd64b3807406a549d8d240122da9682
Seo Jaegon <jgoneit@gmail.com>
2023-07-05 15:50:23 +0900

    feat: Set Base

--- server/src/main/java/com/zerohip/server/financialRecordArticle/entity/FinancialRecordArticle.java
+++ server/src/main/java/com/zerohip/server/financialRecordArticle/entity/FinancialRecordArticle.java
@@ -1,4 +1,7 @@
 package com.zerohip.server.financialRecordArticle.entity;
 
+import javax.persistence.Entity;
+
+@Entity
 public class FinancialRecordArticle {
 }
--- server/src/main/java/com/zerohip/server/financialRecordArticle/repository/FinancialRecordArticleRepository.java
+++ server/src/main/java/com/zerohip/server/financialRecordArticle/repository/FinancialRecordArticleRepository.java
@@ -4,6 +4,5 @@ import com.zerohip.server.financialRecordArticle.entity.FinancialRecordArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/*@Repository
 public interface FinancialRecordArticleRepository extends JpaRepository<FinancialRecordArticle, Long> {
-}*/
+}
