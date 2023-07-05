be2b6c39c65122b3e95d011f419769a150f74909
Seo Jaegon <jgoneit@gmail.com>
2023-07-05 10:16:47 +0900

    feat: FinancialRecordMapper 생성

--- server/src/main/java/com/zerohip/server/financialRecord/mapper/FinancialRecordMapper.java
+++ server/src/main/java/com/zerohip/server/financialRecord/mapper/FinancialRecordMapper.java
@@ -1,2 +1,18 @@
-package com.zerohip.server.financialRecord.mapper;public interface FinancialRecordMapper {
+package com.zerohip.server.financialRecord.mapper;
+
+import com.zerohip.server.financialRecord.dto.FinancialRecordDto;
+import com.zerohip.server.financialRecord.entity.FinancialRecord;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = IGNORE)
+public interface FinancialRecordMapper {
+  FinancialRecord financialRecordPostToFinancialRecord(FinancialRecordDto.Post requestBody);
+  FinancialRecord financialRecordPatchToFinancialRecord(FinancialRecordDto.Patch requestBody);
+  FinancialRecordDto.Response financialRecordToFinancialRecordResponse(FinancialRecord financialRecord);
+  List<FinancialRecordDto.Response> financialRecordsToFinancialRecordResponses(List<FinancialRecord> financialRecords);
 }
