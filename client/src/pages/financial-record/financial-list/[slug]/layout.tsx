ff82650dc3617186c050f36299ece2e784161d6c
dreamogu <dreamogu59@gmail.com>
2023-07-06 15:08:29 +0900

    fix : layout types 추가

--- client/src/pages/financial-record/financial-list/[slug]/layout.tsx
+++ client/src/pages/financial-record/financial-list/[slug]/layout.tsx
@@ -1,6 +1,11 @@
+import { ReactNode } from 'react';
 import RecordHeader from './RecordHeader';
 
-export default function Layout({ children }) {
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <RecordHeader />
