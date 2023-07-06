91eaea49aaffa869c27cc5c7ee91ea32625ab20d
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-06 15:08:12 +0900

    chore: test 파일 삭제 및 아이콘 svg 저장

--- client/src/pages/api/hello.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    pokemon: {
-      name: '꼬부기',
-    },
-  });
-}
