ce9f7e71d2a28ce19f42892ebf9f7ee62abedef4
dreamogu <dreamogu59@gmail.com>
2023-07-07 15:25:04 +0900

    fix : 배포 프리뷰 url 변경에 의한 http 요청  주소 변경

--- client/src/pages/financial-record/FaRecForm.tsx
+++ client/src/pages/financial-record/FaRecForm.tsx
@@ -52,7 +52,7 @@ export default function FaRecForm() {
 
     axios
       .post(
-        'https://zerohip-git-user-55-everland.vercel.app/api/financial-record/',
+        'https://zerohip-r7g30pp7y-everland.vercel.app/api/financial-record/',
         formData,
         {
           headers: {
