d44e2db3f8a107b8dcb6b97f64240a0d3f64e089
YOONHEE-KIM <yhgim1127@gmail.com>
2023-07-07 14:42:59 +0900

    feat: service 패키지 단건조회, 피드조회 추가

--- server/src/main/java/com/zerohip/server/feedArticle/service/FeedArticleServiceImpl.java
+++ server/src/main/java/com/zerohip/server/feedArticle/service/FeedArticleServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zerohip.server.feedArticle.service;
 
+import com.zerohip.server.common.feedType.FeedType;
 import com.zerohip.server.feedArticle.entity.FeedArticle;
 import com.zerohip.server.feedArticle.repository.FeedArticleRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -18,20 +20,38 @@ public class FeedArticleServiceImpl implements FeedArticleService {
     //피드게시글 작성
     @Override
     public FeedArticle createFeedArticle(FeedArticle feedArticle) {
+        validateFeedArticle(feedArticle);
         return feedArticleRepository.save(feedArticle);
     }
 
-    //수정, 조회, 삭제에 대한 로직 구성 시 내용 추가(현재 에러 때문에 메서드만 추가)
+    //피드게시글 작성 요청 시, 피드유형이 아닌 경우 예외 처리 -> 추후 예외처리된 게시글을 가계부게시글 서비스와 연결
+    //다음 이슈처리 시 예외처리를 조금 더 세분화할 예정
+    private void validateFeedArticle(FeedArticle feedArticle) {
+        if (feedArticle.getFeedType() != FeedType.SAVING_TIP && feedArticle.getFeedType() != FeedType.PLEASE) {
+            throw new IllegalArgumentException("가계부 게시글입니다");
+        }
+    }
+
+    @Override
+    public FeedArticle findVerifiedFeedArticle(Long feedArticleId){
+    Optional<FeedArticle> optionalFeedArticle = feedArticleRepository.findById(feedArticleId);
+    FeedArticle findFeedArticle = optionalFeedArticle.orElseThrow(() -> new RuntimeException("게시글이 존재하지 않습니다."));
+    return findFeedArticle;
+    }
+
+    //피드 게시글 조회
     @Override
     public FeedArticle findFeedArticle(Long feedArticleId) {
-        return null;
+        return findVerifiedFeedArticle(feedArticleId);
     }
 
+    //피드 조회
     @Override
     public List<FeedArticle> findFeedArticles() {
-        return null;
+        return feedArticleRepository.findAll();
     }
 
+    //(현재 에러 때문에 메서드만 추가)
     @Override
     public void deleteFeedArticle(Long feedArticleId) {
 
