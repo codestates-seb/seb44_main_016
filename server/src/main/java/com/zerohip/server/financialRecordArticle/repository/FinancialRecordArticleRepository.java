4a89889ddbb995283dd0d7bf3b32fdec5d5e95a4
YOONHEE-KIM <124634807+YOONHEE-KIM@users.noreply.github.com>
2023-07-10 15:37:42 +0900

    fix: 주석해제

--- server/src/main/java/com/zerohip/server/financialRecordArticle/repository/FinancialRecordArticleRepository.java
+++ server/src/main/java/com/zerohip/server/financialRecordArticle/repository/FinancialRecordArticleRepository.java
@@ -4,6 +4,6 @@ import com.zerohip.server.financialRecordArticle.entity.FinancialRecordArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/*@Repository
+@Repository
 public interface FinancialRecordArticleRepository extends JpaRepository<FinancialRecordArticle, Long> {
-}*/
+}
--- server/src/main/java/com/zerohip/server/user/repository/UserRepository.java
+++ server/src/main/java/com/zerohip/server/user/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.zerohip.server.user.repository;
 import com.zerohip.server.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/*public interface UserRepository extends JpaRepository<User, Long> {
-}*/
+public interface UserRepository extends JpaRepository<User, Long> {
+}
