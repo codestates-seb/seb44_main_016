8cb132368703cb7702d0ca69f6a9f1093946c901
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-10 22:14:55 +0900

    feat: 회원가입, 로그입 api 생성

--- client/src/services/apiUser.ts
+++ client/src/services/apiUser.ts
@@ -1,12 +1,32 @@
-import axios from 'axios';
-import { PostSignUp } from '../types/user';
+import axios, { AxiosResponse } from 'axios';
+import { PostSignUp, LoginData } from '../types/user';
 
-export default async function postSignUpData(url: string, data: PostSignUp) {
-  try {
-    const response = await axios.post(url, data);
-    return response.data;
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
-  }
-}
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const userAPI = {
+  /** 회원 가입 */
+  postSignUp: async (signUpData: PostSignUp) => {
+    try {
+      const res = await axios.post(`${BASE_URL}/user/signup`, signUpData);
+      return res.data;
+    } catch (err) {
+      console.error('Error:', err);
+      throw err;
+    }
+  },
+
+  /** 로그인 */
+  apiLogin: async (loginData: LoginData): Promise<AxiosResponse> => {
+    try {
+      const res = await axios.post(`${BASE_URL}/user/login`, loginData, {
+        withCredentials: true,
+      });
+      return res;
+    } catch (err) {
+      console.error('Error:', err);
+      throw err;
+    }
+  },
+};
+
+export default userAPI;
