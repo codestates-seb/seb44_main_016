b051244a20048843bbbecfba5a21688396fe269c
Seo Jaegon <jgoneit@gmail.com>
2023-07-12 10:38:11 +0900

    feat: 가계부 게시글 매핑 추가

--- server/src/main/java/com/zerohip/server/financialRecord/entity/FinancialRecord.java
+++ server/src/main/java/com/zerohip/server/financialRecord/entity/FinancialRecord.java
@@ -1,5 +1,6 @@
 package com.zerohip.server.financialRecord.entity;
 
+import com.zerohip.server.financialRecordArticle.entity.FinancialRecordArticle;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @NoArgsConstructor
 @Setter
@@ -27,13 +29,20 @@ public class FinancialRecord {
   private String memo;
 
   // 게시물 수
-  // 타임라인수 -> 매핑 시 적용
-  // 리스트 매핑
+  // 타임라인수 -> 매핑 시 적용 -> 리스트에 담아서 글 수를 세어야 한다.(or 게시글에서 int형으로 더한다?)
+  @OneToMany(mappedBy = "financialRecord", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<FinancialRecordArticle> financialRecordArticles; // 글 매핑(List 형식)
 
   // 유저 매핑(List 형식)
 
 
   public FinancialRecord(String financialRecordName) {
     this.financialRecordName = financialRecordName;
+  }
+
+  public FinancialRecord(String financialRecordName, String memo, List<FinancialRecordArticle> financialRecordArticles) {
+    this.financialRecordName = financialRecordName;
+    this.memo = memo;
+    this.financialRecordArticles = financialRecordArticles;
   }
 }
