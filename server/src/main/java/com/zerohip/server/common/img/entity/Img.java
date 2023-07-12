bab5a435fcce8f8fd9654a2b567461de244c011c
Seo Jaegon <jgoneit@gmail.com>
2023-07-12 10:37:39 +0900

    feat: img dto, service 추가(미완)

--- server/src/main/java/com/zerohip/server/common/img/entity/Img.java
+++ server/src/main/java/com/zerohip/server/common/img/entity/Img.java
@@ -1,13 +1,14 @@
 package com.zerohip.server.common.img.entity;
 
+import com.zerohip.server.common.article.Article;
+import com.zerohip.server.feedArticle.entity.FeedArticle;
+import com.zerohip.server.financialRecordArticle.entity.FinancialRecordArticle;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @Getter
@@ -18,8 +19,19 @@ public class Img {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Size(max = 50)
+  @Column(nullable = true, length = 50)
   private String fileName;
+  @Size(max = 100)
+  @Column(nullable = true, length = 100)
   private String filePath;
 
   // 매핑 고고
+  @ManyToOne
+  @JoinColumn(name = "fianancial-record-article-id")
+  private FinancialRecordArticle financialRecordArticle;
+
+  /*@ManyToOne
+  @JoinColumn(name = "feed-article-id")
+  private FeedArticle feedArticle;*/
 }
