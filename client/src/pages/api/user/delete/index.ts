597722867889cebdad9666f4e30fdef3137c4643
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-13 15:15:36 +0900

    test: env 테스트 2

--- client/src/pages/api/user/delete/index.ts
+++ client/src/pages/api/user/delete/index.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-// import { userData } from '../../../../constants/userData';
 
 export default function deleteUser(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json('탈퇴가 성공적으로 이루어졌습니다.');
