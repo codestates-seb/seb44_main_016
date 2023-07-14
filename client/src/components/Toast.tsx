07addad6338cc8e7a5cb746007795178e7c26297
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-14 12:07:03 +0900

    chore: 토스트 세팅 확정

--- client/src/components/Toast.tsx
+++ client/src/components/Toast.tsx
@@ -3,16 +3,16 @@ import { ToastContainer } from 'react-toastify';
 export default function Toast() {
   return (
     <ToastContainer
-      position='bottom-right' // 알람 위치 지정
-      autoClose={3000} // 자동 off 시간
-      hideProgressBar={false} // 진행시간바 숨김
+      position='top-right'
+      autoClose={3000}
+      hideProgressBar={false}
       newestOnTop={false}
-      closeOnClick // 클릭으로 알람 닫기
-      rtl={false} // 알림 좌우 반전
-      draggable // 드래그 가능
-      pauseOnHover // 마우스를 올리면 알람 정지
+      closeOnClick
+      rtl={false}
+      draggable
+      pauseOnHover
       theme='light'
-      limit={7} // 알람 개수 제한
+      limit={7}
     />
   );
 }
