4029fad2a43e38ec902802ba1efb8af5126357fb
WAYPIL <ohs3051009@gmail.com>
2023-07-15 00:43:29 +0900

    Revert "Merge remote-tracking branch 'origin/dev' into test-fe"
    This reverts commit 4a4f6e82499316cf68ea5c7011d21d2c67bf9cf0, reversing
    changes made to fff2bc80279866e15a2061bbf25efe045eb87f22.

---
+++ client/src/constants/userData.ts
@@ -0,0 +1,9 @@
+export const userData = [
+  {
+    userId: '12@Sddh4',
+    email: 'doyu@doyu.com',
+    loginId: 'doyu',
+    password: '1111',
+    nickname: 'DoyuMango',
+  },
+];
--- server/src/main/java/com/zerohip/server/financialRecordArticle/repository/FinancialRecordArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zerohip.server.financialRecordArticle.repository;
-
-import com.zerohip.server.financialRecordArticle.entity.FinancialRecordArticle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface FinancialRecordArticleRepository extends JpaRepository<FinancialRecordArticle, Long> { }
