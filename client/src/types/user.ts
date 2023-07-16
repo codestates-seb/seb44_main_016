6c48a53d6764070057f64b3340572bf6c6f4dc6f
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-16 19:46:23 +0900

    chore: 이미지 등 타입 추가

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,3 +1,35 @@
+export interface ValidationValues {
+  loginId?: string | null;
+  pwValue?: string | null;
+  password?: string | null;
+  nickname?: string | null;
+  domainValue?: string | null;
+  emailValue?: string | null;
+  isChecked?: boolean | null;
+}
+export interface SignUpError {
+  loginId: string;
+  password: string;
+  passwordConfirm: string;
+  nickname: string;
+  email: string;
+  policy: string;
+}
+
+export interface UserInputLabel {
+  htmlFor: string;
+  text: string;
+  required?: boolean;
+}
+
+export interface UserInput {
+  label: UserInputLabel;
+  component: React.ReactNode;
+  error: string;
+  subComponent?: boolean;
+  guide?: string;
+}
+
 export interface PostSignUp {
   email: string | null;
   loginId: string | null;
@@ -22,4 +54,11 @@ export interface UserUpdateReqData {
   imgUrl?: string | null;
   nickname?: string | null;
   password?: string | null;
+}
+
+export interface FollowUsersInfoData {
+  imgId: string;
+  isAlsoFollowed: boolean;
+  loginId: string;
+  nickname: string;
 }
