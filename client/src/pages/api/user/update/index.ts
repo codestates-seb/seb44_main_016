adc471049c61f079c7719c0d5ab78d6dfeff875f
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-18 06:56:37 +0900

    test: 배포 테스트

--- client/src/pages/api/user/update/index.ts
+++ client/src/pages/api/user/update/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function signUp(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json('성공!');
+  res.status(200).json('성공');
 }
