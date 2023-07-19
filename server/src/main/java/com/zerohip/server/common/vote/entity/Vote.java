bed321343d8c03adda95e6b3cf9169344df2c13c
YOONHEE-KIM <124634807+YOONHEE-KIM@users.noreply.github.com>
2023-07-19 14:53:58 +0900

    feat: vote엔티티

---
+++ server/src/main/java/com/zerohip/server/common/vote/entity/Vote.java
@@ -0,0 +1,50 @@
+package com.zerohip.server.common.vote.entity;
+
+import com.zerohip.server.common.voteType.VoteType;
+import com.zerohip.server.feedArticle.entity.FeedArticle;
+import com.zerohip.server.user.entity.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "votes")
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_article_id")
+    private FeedArticle feedArticle;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VoteType voteType;
+
+    public Vote() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public FeedArticle getFeedArticle() {
+        return feedArticle;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public VoteType getVoteType() {
+        return voteType;
+    }
+
+    public void setVoteType(VoteType voteType) {
+        this.voteType = voteType;
+    }
+}
