fe8fd3a6ce906b92f12b0060b2205e746fc0c6c4
dreamogu <dreamogu59@gmail.com>
2023-07-20 18:00:42 +0900

    feat : 줌인  라이브러리 설치 및 적용

--- client/src/components/ImgsCarousel.tsx
+++ client/src/components/ImgsCarousel.tsx
@@ -1,12 +1,13 @@
-import React from 'react'; // useState 사용
+import React, { useState } from 'react'; // useState 사용
 import styled from '@emotion/styled';
-
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react'; // Import Swiper React components
 import 'swiper/css'; // Import Swiper styles
 import 'swiper/css/navigation';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 import CommonStyles from '../styles/CommonStyles';
+import SVGs from '../constants/svg';
 
 type Props = {
   imgPath: string[];
@@ -16,6 +17,20 @@ type Props = {
 };
 
 export default function ImgsCarousel(props: Props) {
+  const [modalVisible, setModalVisible] = useState(false); // 모달 상태
+  const [modalImage, setModalImage] = useState(''); // 모달에 보여질 이미
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handleImageClick = (e, imgSrc, index) => {
+    e.stopPropagation();
+    setModalVisible(true);
+    setModalImage(imgSrc);
+    setCurrentSlide(index);
+  };
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    setModalImage('');
+  };
   return (
     <S.ImgContainer>
       {props.rank && (
@@ -42,11 +57,47 @@ export default function ImgsCarousel(props: Props) {
         {props.imgPath.map((imgSrc, i) => {
           return (
             <SwiperSlide key={i}>
-              <S.Img src={imgSrc} alt={`사용자가 올린 ${i + 1}번째 사진`} />
+              <S.Img
+                src={imgSrc}
+                alt={`사용자가 올린 ${i + 1}번째 사진`}
+                onClick={(e) => handleImageClick(e, imgSrc, i)}
+              />
             </SwiperSlide>
           );
         })}
       </Swiper>
+      {modalVisible && (
+        <S.Modal>
+          <S.CloseBtn aria-label='닫기 버튼' onClick={handleCloseModal}>
+            {SVGs.whiteCloseBtn}
+          </S.CloseBtn>
+
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            }}
+            initialSlide={currentSlide}
+          >
+            <S.ImgSlideBtn className='swiper-button-next modal' type='button' />
+            <S.ImgSlideBtn className='swiper-button-prev modal' type='button' />
+            {props.imgPath.map((imgSrc, i) => {
+              return (
+                <SwiperSlide key={i}>
+                  <TransformWrapper initialScale={1} minScale={1} maxScale={10}>
+                    <TransformComponent>
+                      <S.ModalImage src={imgSrc} alt={`사용자가 올린 ${i + 1}번째 사진`} />
+                    </TransformComponent>
+                  </TransformWrapper>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </S.Modal>
+      )}
     </S.ImgContainer>
   );
 }
@@ -86,6 +137,9 @@ const S = {
     &.swiper-button-disabled {
       visibility: hidden;
     }
+    &.modal.swiper-button-next {
+      right: 23px;
+    }
   `,
 
   RankIndicator: styled.div`
@@ -98,11 +152,48 @@ const S = {
     align-items: end;
     padding-right: 3rem;
     padding-bottom: 2rem;
-    z-index: 996;
+    z-index: 9999;
   `,
   RankText: styled.h2`
     font-size: 3rem;
     color: white;
     border-bottom: 0.3rem solid white;
+  `,
+  Modal: styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 999;
+
+    & > button.swiper-button-next {
+    }
+  `,
+
+  ModalImage: styled.img`
+    max-width: 100%;
+    max-height: 100%;
+  `,
+  CloseBtn: styled.button`
+    position: absolute;
+    top: 1rem;
+    right: 2rem;
+    width: 2rem;
+    height: 2rem;
+    z-index: 1000;
+    transition-duration: 0.3s;
+    & > svg {
+      width: 100%;
+      height: 100%;
+      color: #fff;
+    }
+    &:active {
+      transform: scale(0.95);
+    }
   `,
 };
