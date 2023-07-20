211d5f0d13bf4a278ef709893607f09a4bac3a81
Seo Jaegon <jgoneit@gmail.com>
2023-07-20 16:52:28 +0900

    issue: client git 추적 해제

--- client/src/pages/api/user/delete/index.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function deleteUser(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json('탈퇴가 성공적으로 이루어졌습니다.');
-}
