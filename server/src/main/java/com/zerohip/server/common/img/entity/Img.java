f8ba5d3b47fe3358e49ccba41feec7b6ab342eb4
Seo Jaegon <jgoneit@gmail.com>
2023-07-21 11:14:09 +0900

    fix: aws s3업로드 및 매핑에 의한 수정

--- server/src/main/java/com/zerohip/server/common/img/entity/Img.java
+++ server/src/main/java/com/zerohip/server/common/img/entity/Img.java
@@ -1,38 +1,46 @@
-//package com.zerohip.server.common.img.entity;
-//
+package com.zerohip.server.common.img.entity;
 
-//import com.zerohip.server.common.article.Article;
-//import com.zerohip.server.feedArticle.entity.FeedArticle;
-//import com.zerohip.server.financialRecordArticle.entity.FinancialRecordArticle;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//import javax.validation.constraints.Size;
-//
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@Entity
-//public class Img {
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long id;
-//
-//  @Size(max = 50)
-//  @Column(nullable = true, length = 50)
-//  private String fileName;
-//  @Size(max = 100)
-//  @Column(nullable = true, length = 100)
-//  private String filePath;
-//
-//  // 매핑 고고
-//  @ManyToOne
-//  @JoinColumn(name = "fianancial-record-article-id")
-//  private FinancialRecordArticle financialRecordArticle;
-//
-//  @ManyToOne
-//  @JoinColumn(name = "feed-article-id")
-//  private FeedArticle feedArticle;
-//}
+
+import com.zerohip.server.common.article.Article;
+import com.zerohip.server.feedArticle.entity.FeedArticle;
+import com.zerohip.server.financialRecordArticle.entity.FinancialRecordArticle;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "imgs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Img {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Size(max = 50)
+  @Column(nullable = true, length = 50)
+  private String fileName;
+  @Size(max = 100)
+  @Column(nullable = true, length = 100)
+  private String filePath;
+
+  // 매핑 고고
+  @ManyToOne
+  @JoinColumn(name = "fianancial_record_article_id")
+  private FinancialRecordArticle financialRecordArticle;
+
+  @ManyToOne
+  @JoinColumn(name = "feed_article_id")
+  private FeedArticle feedArticle;
+
+  public Img(String fileName, String filePath) {
+    this.fileName = fileName;
+    this.filePath = filePath;
+  }
+}
--- server/src/main/java/com/zerohip/server/common/img/service/S3ImgServiceImpl.java
+++ server/src/main/java/com/zerohip/server/common/img/service/S3ImgServiceImpl.java
@@ -1,2 +1,81 @@
-package com.zerohip.server.common.img.service;public class S3ImgServiceImpl {
+package com.zerohip.server.common.img.service;
+
+import com.zerohip.server.aws.config.S3Uploader;
+import com.zerohip.server.common.article.Article;
+import com.zerohip.server.common.img.dto.ImgDto;
+import com.zerohip.server.common.img.entity.Img;
+import com.zerohip.server.common.img.repository.ImgRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.zerohip.server.common.img.service.ImgServiceImpl.setArticle;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Primary
+public class S3ImgServiceImpl implements ImgService {
+
+  private final S3Uploader s3Uploader;
+  private final ImgRepository imgRepository;
+  @Override
+  public List<Img> createImg(Article article, List<MultipartFile> files) throws IOException {
+    List<Img> imgList = new ArrayList<>();
+
+    String dirName = "zerohip"; // 해당 이미지를 저장할 S3 bucket의 디렉토리 이름을 지정하세요.
+
+    for (MultipartFile file : files) {
+      // S3에 파일을 업로드하고, 그 URL을 가져옵니다.
+      List<String> imageUrlList = s3Uploader.uploadFiles(List.of(file), dirName);
+
+      // 이미지 생성자로 파일명, 파일경로 넘겨줌
+      Img img = setImg(imageUrlList.get(0));
+
+      // 게시판 글과 이미지 연결
+      setArticle(article, img);
+
+      // 파일 경로를 리스트에 저장
+      imgList.add(img);
+    }
+    return imgRepository.saveAll(imgList);
+  }
+
+  private static Img setImg(String imageUrl) {
+    Img img = new Img();
+    img.setFileName(imageUrl.substring(imageUrl.lastIndexOf("/") + 1));  // URL의 마지막 부분을 파일 이름으로 사용합니다.
+    img.setFilePath(imageUrl); // 전체 URL을 파일 경로로 사용합니다.
+    return img;
+  }
+
+  @Override
+  public Img findImg(Long imgId) {
+    return null;
+  }
+
+  @Override
+  public List<Img> findImgs() {
+    return null;
+  }
+
+  @Override
+  public void updateImg(Long imgId, ImgDto.Patch patchParam) {
+
+  }
+
+  @Override
+  public void deleteImg(Long imgId) {
+
+  }
+
+  @Override
+  public Img findVerifiedImg(Long imgId) {
+    return null;
+  }
 }
