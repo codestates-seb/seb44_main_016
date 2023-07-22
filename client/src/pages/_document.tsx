2f0bc3c82d8eaca05a4645e6b3d6c50283c88940
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-23 01:55:58 +0900

    fix: document 파일에 title 제거

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -7,7 +7,6 @@ const Document = () => {
       <Head>
         <meta charSet='UTF-8' />
         <link rel='icon' href='/images/favicon.ico' />
-        <title>제로힙</title>
       </Head>
       <body>
         <Main />
