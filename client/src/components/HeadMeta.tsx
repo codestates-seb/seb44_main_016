4fc5f69b63e5d4ee2e787629864676f481083838
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-23 00:11:58 +0900

    test : og 이미지 경로 수정

--- client/src/components/HeadMeta.tsx
+++ client/src/components/HeadMeta.tsx
@@ -24,7 +24,13 @@ export default function HeadMeta({ title, description, url, image }: HeadMetaPro
         content={description || '절약유도 가계부형 SNS | 당신의 과소비가 0으로 수렴할 때까지, 제로힙.'}
       />
       <meta property='og:url' content={url || 'https://zerohip.co.kr'} />
-      <meta property='og:image' content={image || '/images/image/zerohipMetaImg'} />
+      <meta
+        property='og:image'
+        content={
+          image ||
+          'https://media.discordapp.net/attachments/1123431285671612496/1132326412343332864/zerohipMetaImg.png?width=2175&height=1222'
+        }
+      />
       <meta property='og:article:author' content='제로힙' />
 
       <meta name='twitter:card' content='summary_large_image' />
