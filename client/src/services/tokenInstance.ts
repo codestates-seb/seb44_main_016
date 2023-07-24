8f77cfee059a62b8c700f5b858b0718e7b2b6053
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-24 18:36:24 +0900

    fix: if문 조건 수정

--- client/src/services/tokenInstance.ts
+++ client/src/services/tokenInstance.ts
@@ -37,7 +37,7 @@ instance.interceptors.response.use(
       const originalRequest = config;
       console.log('액세스 없어세 에러났어 ');
       console.log(error);
-      if (error) {
+      if (response) {
         /** GET : NEW ACCESS TOKEN */
         const res = await axios.post(`/auth/refresh`, null, {
           headers: {
