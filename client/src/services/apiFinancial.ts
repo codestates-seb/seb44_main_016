4ec6785bf37b4f4ee22e4a42c97352a3228bd853
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-24 21:15:23 +0900

    test: 배포 테스트

--- client/src/services/apiFinancial.ts
+++ client/src/services/apiFinancial.ts
@@ -7,6 +7,7 @@ export const APIfinancialRecord = {
   getRecordList: async () => {
     const res = await instance.get(`${BASE_URL}/financial-record`, {
       withCredentials: true,
+      timeout: 5000,
     });
     // const res = await axios.get('/data/recordList.json');
     return res.data;
