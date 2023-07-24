7c624e3c4514ecbb0e0a98593f81938a670947d3
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-24 23:54:48 +0900

    test: isLoggedIn 테스트

--- client/src/pages/user/mypage/index.tsx
+++ client/src/pages/user/mypage/index.tsx
@@ -13,10 +13,13 @@ import { USER_META_DATA } from '../../../constants/seo/userMetaData';
 import ErrorComponent from '../../../components/ErrorComponent';
 import { useEffect } from 'react';
 import MyPageUserInfo from './MyPageUserInfo';
-import { userInfo } from '../../../components/redux/getUserInfo';
+import useGlobalUserInfo, { userInfo } from '../../../components/redux/getUserInfo';
 
 function MyPage() {
   const { ref, inView } = useInView();
+
+  const isLoggedIn = useGlobalUserInfo();
+  console.log(isLoggedIn);
 
   const {
     isLoading: isMyInfoLoading,
