fdcbeb083ac5e7f4e0123484c0570af0b15ee42d
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-24 16:20:29 +0900

    fix: 쿼리 무효화 추가

--- client/src/components/WithAuth.tsx
+++ client/src/components/WithAuth.tsx
@@ -3,20 +3,19 @@ import { ComponentType } from 'react';
 import { useRouter } from 'next/router';
 import { useAppDispatch } from './redux/hooks';
 import { login, logout } from './redux/authnReducer';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import apiUser from '../services/apiUser';
 import useAccessToken from './redux/getUserInfo';
 
 const withAuth = (Component: ComponentType) => (props: object) => {
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const queryClient = useQueryClient();
 
   const accessToken = useAccessToken();
   // const accessToken = null; // accessToken 만료 test용 (배포 직전 삭제)
 
-  const { data: myInfoData } = useQuery(['myInfo'], apiUser.getUserInfo, {
-    enabled: !!accessToken,
-  });
+  const { data: myInfoData } = useQuery(['myInfo'], apiUser.getUserInfo);
 
   /** refresh 토큰으로 새 access 토큰을 발급받는 api */
   const { mutate } = useMutation(apiUser.getNewAccess, {
@@ -39,6 +38,10 @@ const withAuth = (Component: ComponentType) => (props: object) => {
   }, []);
 
   useEffect(() => {
+    if (accessToken) {
+      queryClient.invalidateQueries(['myInfo']);
+    }
+
     if (myInfoData) {
       console.log('myInfoData있어.');
       console.log(myInfoData);
@@ -48,7 +51,7 @@ const withAuth = (Component: ComponentType) => (props: object) => {
       console.log(userId, loginId, nickname);
       dispatch(login({ userId, loginId, nickname }));
     }
-  }, [myInfoData]);
+  }, [accessToken, myInfoData]);
 
   return <Component {...props} />;
 };
