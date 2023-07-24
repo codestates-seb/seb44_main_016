963c08b9193338e9e7999c86223730f067b2f27d
WAYPIL <ohs3051009@gmail.com>
2023-07-25 00:05:16 +0900

    Revert "chore: 내 가계부 href 추가 및 안 쓰는 부분 주석화"
    This reverts commit e7c08257490f1f606e754689006a5e783b19872d.

--- client/src/components/Aside.tsx
+++ client/src/components/Aside.tsx
@@ -67,17 +67,16 @@ export default function Aside(props: Props) {
               <AsideBtn onClick={handleOpenOrCloseNoticeTab} leftIcon={svgs.notice} className={asideClsName}>
                 알림
               </AsideBtn>
+              {/* href='/financial-record' */}
               <AsideBtn
-                href='/financial-record'
                 leftIcon={svgs.faRec}
-                // rightIcon={svgs.dropdown}
+                rightIcon={svgs.dropdown}
                 isReverse={isBookmarkedFaRecListOpened}
                 onClickRight={handleOpenOrCloseBookmarkedFaRecList}
                 className={asideClsName}
               >
                 내 가계부
               </AsideBtn>
-              {/*
               {isBookmarkedFaRecListOpened || isShrinkOrMobile ? (
                 <></>
               ) : (
@@ -93,7 +92,6 @@ export default function Aside(props: Props) {
                   </AsideBtn>
                 </ol>
               )}
-              */}
             </>
           )}
           <AsideBtn onClick={handleOpenOrCloseSearchTab} leftIcon={svgs.search} className={asideClsName}>
