4125cc3219c0d3d8dad613e4f07792a4f4a5db20
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-24 18:14:49 +0900

    fix: 인스턴스 로직 변경

--- client/src/services/tokenInstance.ts
+++ client/src/services/tokenInstance.ts
@@ -25,7 +25,7 @@ instance.interceptors.request.use(
 
 /** RESPONSE INTERCEPTORS */
 instance.interceptors.response.use(
-  (response) => {
+  async (response) => {
     console.log('리스펀스!');
 
     return response;
@@ -38,7 +38,6 @@ instance.interceptors.response.use(
       console.log(error);
       /** GET : NEW ACCESS TOKEN */
       const res = await axios.post(`/auth/refresh`, null, {
-        // 서버 배포 링크로 추후 변경 예정
         headers: {
           'Content-Type': 'application/json',
         },
@@ -48,13 +47,12 @@ instance.interceptors.response.use(
       /** CHANGE ACCESS TOKEN AND RETRY THE REQUEST*/
       originalRequest.headers['Authorization'] = res.headers.authorization;
       console.log('액세스 없어서 다시 받아왔음!!');
-      console.log(res.headers);
+      console.log(res);
 
       return axios(originalRequest);
     } catch (error) {
       toast.info('로그인이 필요한 서비스입니다.');
       return false;
     }
-    return Promise.reject(error);
   }
 );
