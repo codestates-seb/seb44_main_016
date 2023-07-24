5289b060dbf67d3972f468c56967bf88dd51c064
Seo Jaegon <jgoneit@gmail.com>
2023-07-24 13:19:15 +0900

    feat: comment Mapper 추가

--- server/src/main/java/com/zerohip/server/common/comment/mapper/CommentMapper.java
+++ server/src/main/java/com/zerohip/server/common/comment/mapper/CommentMapper.java
@@ -1,2 +1,21 @@
-package com.zerohip.server.common.comment.mapper;public class CommentMapper {
+package com.zerohip.server.common.comment.mapper;
+
+import com.zerohip.server.common.comment.dto.CommentDto;
+import com.zerohip.server.common.comment.entity.Comment;
+import org.mapstruct.Mapper;
+
+import javax.swing.*;
+
+import java.util.List;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = IGNORE)
+public abstract class CommentMapper {
+
+  public abstract Comment commentPostToComment(CommentDto.Post data);
+  public abstract Comment commentPatchToComment(CommentDto.Patch data);
+  public abstract CommentDto.Response commentToCommentResponse(Comment comment);
+  public abstract List<CommentDto.Response> commentsToCommentResponses(List<Comment> comments);
 }
