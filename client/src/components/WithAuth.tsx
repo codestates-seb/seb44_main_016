0fc8859eb6733cb551903d7899cde168dd65d0f1
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-25 19:25:00 +0900

    fix: accessToken 및 query 로직 변경

--- client/src/components/WithAuth.tsx
+++ client/src/components/WithAuth.tsx
@@ -3,7 +3,7 @@ import { ComponentType } from 'react';
 import { useRouter } from 'next/router';
 import { useAppDispatch } from './redux/hooks';
 import { login, logout } from './redux/authnReducer';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import apiUser from '../services/apiUser';
 import useUserGlobalValue from './redux/getUserInfo';
 
@@ -12,17 +12,17 @@ const withAuth = (Component: ComponentType) => (props: object) => {
   const dispatch = useAppDispatch();
 
   const { accessToken } = useUserGlobalValue();
-  // const accessToken = null; // accessToken 만료 test용 (배포 직전 삭제)
-  const [newAccessToken, setNewAccessToken] = useState(accessToken);
+  const [changedAccessToken, setChangedAccessToken] = useState(null);
 
-  const { data: myInfoData } = useQuery(['userInfo'], apiUser.getUserInfo, { enabled: !!accessToken });
+  const { data: myInfoData } = useQuery(['userInfo'], apiUser.getUserInfo, { enabled: !!changedAccessToken });
+  console.log(myInfoData);
 
   /** refresh 토큰으로 새 access 토큰을 발급받는 api */
   const { mutate } = useMutation(apiUser.getNewAccess, {
     onSuccess: (data) => {
       const newAccessTokenWithBearer = data.headers.authorization;
       const newAccessToken = newAccessTokenWithBearer.split(' ')[1];
-      setNewAccessToken(newAccessToken);
+      setChangedAccessToken(newAccessToken);
     },
     onError: (err) => {
       // dispatch(logout()); 배포 때 살릴 예정
@@ -34,10 +34,18 @@ const withAuth = (Component: ComponentType) => (props: object) => {
   useEffect(() => {
     if (!accessToken) {
       mutate();
-    } else if (newAccessToken && myInfoData) {
+    } else if (changedAccessToken && myInfoData) {
       const { userId, loginId, nickname, profileImgPath } = myInfoData;
+      console.log(profileImgPath);
       dispatch(
-        login({ userId, loginId, nickname, accessToken: newAccessToken, profileImgPath, isLoggedIn: true })
+        login({
+          userId,
+          loginId,
+          nickname,
+          accessToken: changedAccessToken,
+          profileImgPath,
+          isLoggedIn: true,
+        })
       );
     }
   }, [accessToken, myInfoData]);
