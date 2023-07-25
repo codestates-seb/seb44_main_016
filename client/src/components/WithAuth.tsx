c013e1253814d23765c1ec83f1ffeaff027f0951
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-25 20:02:35 +0900

    fix: useEffect if문 수정

--- client/src/components/WithAuth.tsx
+++ client/src/components/WithAuth.tsx
@@ -31,11 +31,12 @@ const withAuth = (Component: ComponentType) => (props: object) => {
   });
 
   useEffect(() => {
-    if (!accessToken) {
+    if (!changedAccessToken) {
       mutate();
     } else if (changedAccessToken && myInfoData) {
       const { userId, loginId, nickname, profileImgPath } = myInfoData;
       console.log(profileImgPath);
+      console.log('here!');
       dispatch(
         login({
           userId,
