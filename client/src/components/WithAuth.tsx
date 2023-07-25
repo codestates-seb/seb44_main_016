768c50f7a5db87e9eee669b074a682e40dc35171
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-25 18:56:59 +0900

    fix: useQuery 설정 변경

--- client/src/components/WithAuth.tsx
+++ client/src/components/WithAuth.tsx
@@ -16,7 +16,7 @@ const withAuth = (Component: ComponentType) => (props: object) => {
   // const accessToken = null; // accessToken 만료 test용 (배포 직전 삭제)
   const [accessToken, setAccessToken] = useState(null);
 
-  const { data: myInfoData } = useQuery(['userInfo'], apiUser.getUserInfo);
+  const { data: myInfoData } = useQuery(['userInfo'], apiUser.getUserInfo, { enabled: !!accessToken });
 
   /** refresh 토큰으로 새 access 토큰을 발급받는 api */
   const { mutate } = useMutation(apiUser.getNewAccess, {
@@ -36,8 +36,6 @@ const withAuth = (Component: ComponentType) => (props: object) => {
     if (!accessToken) {
       mutate();
     } else if (accessToken) {
-      console.log(accessToken);
-      queryClient.invalidateQueries(['userInfo']);
       const { userId, loginId, nickname, profileImgPath } = myInfoData;
       console.log(profileImgPath);
       dispatch(login({ userId, loginId, nickname, accessToken, profileImgPath, isLoggedIn: true }));
