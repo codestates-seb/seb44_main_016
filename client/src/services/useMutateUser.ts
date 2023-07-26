7e74fbff5299e6d05c4c1b47cffe8d8bed31269a
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-07-26 22:04:51 +0900

    test: 배포 테스트 (주석 제거)

--- client/src/services/useMutateUser.ts
+++ client/src/services/useMutateUser.ts
@@ -42,7 +42,6 @@ const useMutateUser = {
     const dispatch = useAppDispatch();
     const { mutate, data } = useMutation(['login'], mutateFunction, {
       onSuccess: (data) => {
-        console.log(data);
         if (data.status === 200) {
           const accessTokenWithBearer = data.headers.authorization;
           const accessToken = accessTokenWithBearer.split(' ')[1];
